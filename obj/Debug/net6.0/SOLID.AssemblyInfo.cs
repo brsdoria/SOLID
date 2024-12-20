9fcdc8d1bc3e28c11a39780ec641461ac457c0b7
Byron Doria <brsdoria@gmail.com>
2024-12-20 20:44:22 -0300

    commit realizado

--- obj/Debug/net6.0/SOLID.AssemblyInfo.cs
+++ obj/Debug/net6.0/SOLID.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SOLID")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e50751e101ac31f6bbf188f9eba0478da8f687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c030b510678f6e0c98993677e8f1422ae25034fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SOLID")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SOLID")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
